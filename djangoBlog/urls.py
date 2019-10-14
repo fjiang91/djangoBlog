ec86cd38541f43bfe963476987e7abfd955fd781
Feng Jiang <fjiang91@gmail.com>
2019-10-14 17:45:51 -0400

    feature: added media

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('', views.homepage),
 ]
 
+print(settings.MEDIA_URL , 'url setting')
+print(settings.MEDIA_ROOT, 'root setting')
+
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
